d158e89293a5388d39acb3ae979a9e1051d14be1
satya <sroutraykec@gmail.com>
2021-02-01 21:58:59 +0530

    home page

--- lib/ui/pages/home.dart
+++ lib/ui/pages/home.dart
@@ -1,10 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 import 'package:opentrivia/models/category.dart';
+import 'package:opentrivia/ui/pages/quiz_page.dart';
 import 'package:opentrivia/ui/widgets/quiz_options.dart';
 import 'package:auto_size_text/auto_size_text.dart';
+import 'package:opentrivia/models/question.dart';
+import 'package:opentrivia/resources/api_provider.dart';
 
-class HomePage extends StatelessWidget {
+import 'error.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final List<Color> tileColors = [
     Colors.green,
     Colors.blue,
@@ -22,17 +34,25 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          backgroundColor: Color(0xFF303F9F),
           title: Text('Quiz App'),
           elevation: 0,
         ),
         body: Stack(
           children: <Widget>[
             ClipPath(
-              clipper: WaveClipperTwo(),
               child: Container(
-                decoration:
-                    BoxDecoration(color: Theme.of(context).primaryColor),
-                height: 200,
+                decoration: BoxDecoration(
+                  gradient: new LinearGradient(
+                      colors: [
+                        const Color(0xFF303F9F),
+                        const Color(0xFF1F1147),
+                      ],
+                      begin: const FractionalOffset(0.0, 0.0),
+                      end: const FractionalOffset(1.0, 0.0),
+                      stops: [0.0, 1.0],
+                      tileMode: TileMode.repeated),
+                ),
               ),
             ),
             CustomScrollView(
@@ -79,10 +99,13 @@ class HomePage extends StatelessWidget {
       elevation: 1.0,
       highlightElevation: 1.0,
       onPressed: () {
-        _categoryPressed(context, category);
+        //_categoryPressed(context, category);
+
+        _startQuiz(category);
       },
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
+      shape: CircleBorder(
+        side: BorderSide.none
+        //borderRadius: BorderRadius.circular(10.0),
       ),
       color: Colors.white,//Colors.grey.shade800,
       textColor: Colors.blue,
@@ -93,6 +116,7 @@ class HomePage extends StatelessWidget {
           if (category.icon != null) SizedBox(height: 5.0),
           AutoSizeText(
             category.name,
+
             minFontSize: 10.0,
             textAlign: TextAlign.center,
             maxLines: 3,
@@ -107,11 +131,53 @@ class HomePage extends StatelessWidget {
     showModalBottomSheet(
       context: context,
       builder: (sheetContext) => BottomSheet(
-        builder: (_) => QuizOptionsDialog(
-          category: category,
-        ),
+        builder: (_) {
+          return QuizOptionsDialog(
+            category: category,
+          );
+        },
         onClosing: () {},
       ),
     );
   }
+
+
+  void _startQuiz(Category category) async {
+    setState(() {
+      //processing=true;
+    });
+    try {
+      List<Question> questions =  await getQuestions(category, 10, "easy");
+      Navigator.pop(context);
+      if(questions.length < 1) {
+        Navigator.of(context).push(MaterialPageRoute(
+            builder: (_) {
+              return ErrorPage(
+                message: "There are not enough questions in the category, with the options you selected.",);
+            }
+        ));
+        return;
+      }
+      Navigator.push(context, MaterialPageRoute(
+          builder: (_) => QuizPage(questions: questions, category: category,)
+      ));
+    }on SocketException catch (_) {
+      Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (_) {
+            return ErrorPage(message: "Can't reach the servers, \n Please check your internet connection.",);
+          }
+      ));
+    } catch(e){
+      print(e.message);
+      Navigator.pushReplacement(context, MaterialPageRoute(
+          builder: (_) {
+            return ErrorPage(message: "Unexpected error trying to connect to the API",);
+          }
+      ));
+    }
+    setState(() {
+      //processing=false;
+    });
+  }
 }
+
