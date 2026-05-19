a178019999d11dbb5095de714defef2820e60174
satya <sroutraykec@gmail.com>
2026-05-19 10:02:17 +0530

    latest version

---
+++ android/app/src/main/kotlin/satya/routray/quizsquare/MainActivity.kt
@@ -0,0 +1,12 @@
+package satya.routray.quizsquare
+
+import android.os.Bundle
+
+//import io.flutter.app.FlutterActivity
+//import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+
+class MainActivity: FlutterActivity() {
+
+}
